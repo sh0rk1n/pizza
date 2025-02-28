7660d674b07b752393fdb0a4e16f97482c66ccb8
sh0rk1n <sh0rk1n@mail.ru>
2025-02-28 11:57:58 +0300

    refactor:  use next router group

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import { Header } from "@/components/shared/header";
 import "./globals.css";
 
 const nunito = Nunito({
@@ -9,10 +7,6 @@ const nunito = Nunito({
   weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata: Metadata = {
-  title: "Pizza",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -20,12 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={nunito.className}>
-        <main className="min-h-screen">
-          <Header />
-          {children}
-        </main>
-      </body>
+      <head>
+        <link data-rh="true" rel="icon" href="/logo.png" />
+      </head>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
